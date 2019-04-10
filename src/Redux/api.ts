1767d2007de88a135f3eebbd07700fea90432cd9
creatrixity <creatrixity@gmail.com>
2019-04-10 09:04:56 +0100

    Update api.ts

--- src/Redux/api.ts
+++ src/Redux/api.ts
@@ -2,7 +2,7 @@ import { Dispatch } from 'redux';
 import { AppActions } from '@Actions';
 import * as utils from './utils'
 
-const hostAddress = 'http://localhost:3333/api';
+const hostAddress = 'http://127.0.0.1:3333/api';
 
 function prefixHostAddress (url:string) {
   return `${hostAddress}${url}`;
