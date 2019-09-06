3fb0abaad487ce92aaa6baf7da9d21373491ad3b
Caleb Mathew <creatrixity@gmail.com>
2019-09-06 18:14:17 +0100

    update endpoint

--- out/service-worker.js
+++ out/service-worker.js
@@ -24,92 +24,92 @@ self.__precacheManifest = [
     "revision": "44b43abd9436a734f341"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\_app.js",
-    "revision": "0e3f40c4437304e235c3"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\_app.js",
+    "revision": "b8f924ceccb1fb1743aa"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\_error.js",
-    "revision": "90b7d5707942a237cfb9"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\_error.js",
+    "revision": "6bfb255a1153346a929b"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\courses.js",
-    "revision": "ec2fe9679a175f92e5b9"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\courses.js",
+    "revision": "5b93a8ee9375662ed841"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\courses\\course.js",
-    "revision": "2051e582b19072e6b0e4"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\courses\\course.js",
+    "revision": "59befc7ed2ab094197d3"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\courses\\course\\course.js",
-    "revision": "105d69b31a1338c07447"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\courses\\course\\course.js",
+    "revision": "755b6523c344a9fd7203"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\courses\\course\\module.js",
-    "revision": "bd10cc385fa72ddedad5"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\courses\\course\\module.js",
+    "revision": "c062dd8e6396da8e32b6"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\courses\\course\\module\\data.js",
-    "revision": "daa0615dcbe3e4717b0c"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\courses\\course\\module\\data.js",
+    "revision": "af6ff53ea1892ce31dbd"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\courses\\course\\module\\lesson.js",
-    "revision": "d739b2257a46d435563f"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\courses\\course\\module\\lesson.js",
+    "revision": "803946e93648ddba5470"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\courses\\course\\module\\lesson\\lesson.js",
-    "revision": "321a97d31b88bb552440"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\courses\\course\\module\\lesson\\lesson.js",
+    "revision": "82f289fe32dc6b0fdbc6"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\courses\\course\\module\\module.js",
-    "revision": "a85dcf06f60078bf8fd6"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\courses\\course\\module\\module.js",
+    "revision": "4bbbd09e3676b374bb34"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\courses\\courses.js",
-    "revision": "895d20ea443011d34cde"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\courses\\courses.js",
+    "revision": "864c41809ef8f26e0781"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\home.js",
-    "revision": "334932ef1b52223ea2bf"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\home.js",
+    "revision": "f44e28dddd12a1ba21a2"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\home\\home.js",
-    "revision": "e41c60b3ca2b7468d43d"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\home\\home.js",
+    "revision": "3b30bd3e6eac09f5b0a1"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\index.js",
-    "revision": "dcefde632c3ea8aa17cb"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\index.js",
+    "revision": "2c529fe4978992320748"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\login.js",
-    "revision": "105ef85831121f3a7c64"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\login.js",
+    "revision": "14115c6ab1c0fd36f9b1"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\login\\login.js",
-    "revision": "801b60560e96ed3223ea"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\login\\login.js",
+    "revision": "2e28700231fff92bbc1f"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\profile.js",
-    "revision": "4658a15fda9509c1511a"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\profile.js",
+    "revision": "f5e9e75ee9741bb12182"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\profile\\profile.js",
-    "revision": "acee2e90d45301e9bb3f"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\profile\\profile.js",
+    "revision": "ba3dde674cf8cface533"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\register.js",
-    "revision": "494954923eea1b3c4277"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\register.js",
+    "revision": "f7f964548e0b3caeed14"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\register\\register.js",
-    "revision": "04d29b11e75fb699698e"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\register\\register.js",
+    "revision": "8f35cb0032cd10c7a960"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\welcome.js",
-    "revision": "38fe83f68b24b2106ad4"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\welcome.js",
+    "revision": "f969bfdb1667447a1502"
   },
   {
-    "url": "/_next/static\\~LBHbsh47R6OBlLjxSDrS\\pages\\welcome\\welcome.js",
-    "revision": "4966a8572aa41937842a"
+    "url": "/_next/static\\4eiTqdaTRlDHY0V1UjAtW\\pages\\welcome\\welcome.js",
+    "revision": "d73848531c6d261fa88b"
   }
 ];
 
--- src/Redux/api.ts
+++ src/Redux/api.ts
@@ -1,117 +1,125 @@
 import { Dispatch } from 'redux';
 import { AppActions } from '@Actions';
 import fetch from 'isomorphic-fetch';
-import * as utils from './utils'
+import * as utils from './utils';
 import getConfig from 'next/config';
-const qs = require('query-string')
+const qs = require('query-string');
 
-const { publicRuntimeConfig: { envSpecifics } } = getConfig();
+const {
+  publicRuntimeConfig: { envSpecifics }
+} = getConfig();
 
 const hostAddress = envSpecifics.api;
 
-function prefixHostAddress (url:string) {
-  return `${hostAddress}${url}`;
+function prefixHostAddress(url: string) {
+  return `${hostAddress}api/${url}`;
 }
 
 /**
  * Creates a user http request.
  */
-function createUser (user:Object) {
+function createUser(user: Object) {
   return fetch(prefixHostAddress('/v1/register'), {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(user)
-  })
+  });
 }
 
 /**
  * Creates an entry in the journal.
  */
-function createJournalFeedback (payload:Object) {
+function createJournalFeedback(payload: Object) {
   return fetch(prefixHostAddress('/v1/feedback'), {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(payload)
-  })
+  });
 }
 
 /**
  * Fetches journal feedback
  */
-function getJournalFeedback (payload:Object) {
+function getJournalFeedback(payload: Object) {
   return fetch(prefixHostAddress('/v1/feedback/getFeedback'), {
     method: 'post',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${utils.getToken()}`
+      Authorization: `Bearer ${utils.getToken()}`
     },
     body: JSON.stringify(payload)
-  })
+  });
 }
 
 /**
  * Updates a user module.
  */
-function updateUserModule (payload:Object) {
+function updateUserModule(payload: Object) {
   return fetch(prefixHostAddress('/v1/modules/updateUserModule'), {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${utils.getToken()}`
+      Authorization: `Bearer ${utils.getToken()}`
     },
     body: JSON.stringify(payload)
-  })
+  });
 }
 
 /**
  * Creates a user http request.
  */
-function updateUserLesson (payload:Object) {
-  return fetch(prefixHostAddress(`/v1/modules/updateUserLesson?${qs.stringify(payload)}`), {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${utils.getToken()}`
-    },
-  })
-}
-
-/**
- * Creates a user http request.
- */
-function getUserLessons (payload:Object) {
-  return fetch(prefixHostAddress(`/v1/modules/getUserLessons/?${qs.stringify(payload)}`), {
-    method: 'get',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${utils.getToken()}`
+function updateUserLesson(payload: Object) {
+  return fetch(
+    prefixHostAddress(`/v1/modules/updateUserLesson?${qs.stringify(payload)}`),
+    {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${utils.getToken()}`
+      }
     }
-  })
+  );
 }
 
 /**
  * Creates a user http request.
  */
-function getUserModules (payload:Object) {
+function getUserLessons(payload: Object) {
+  return fetch(
+    prefixHostAddress(`/v1/modules/getUserLessons/?${qs.stringify(payload)}`),
+    {
+      method: 'get',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${utils.getToken()}`
+      }
+    }
+  );
+}
+
+/**
+ * Creates a user http request.
+ */
+function getUserModules(payload: Object) {
   return fetch(prefixHostAddress('/v1/modules/getUserModules'), {
     method: 'post',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${utils.getToken()}`
+      Authorization: `Bearer ${utils.getToken()}`
     },
     body: JSON.stringify(payload)
-  })
+  });
 }
 
 /**
@@ -119,16 +127,16 @@ function getUserModules (payload:Object) {
  * @param {Object} payload
  * @returns Promise<Object>
  */
-function fetchCourseModuleLessonView (payload:Object) {
+function fetchCourseModuleLessonView(payload: Object) {
   return fetch(prefixHostAddress('/v1/fetchModuleLessonView'), {
     method: 'post',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${utils.getToken()}`
+      Authorization: `Bearer ${utils.getToken()}`
     },
     body: JSON.stringify(payload)
-  })
+  });
 }
 
 /**
@@ -136,66 +144,66 @@ function fetchCourseModuleLessonView (payload:Object) {
  * @param {Object} payload
  * @returns Promise<Object>
  */
-function fetchLessonsByModuleId(payload:any) {
+function fetchLessonsByModuleId(payload: any) {
   return fetch(prefixHostAddress(`/v1/modules/${payload.moduleId}`), {
     method: 'post',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${utils.getToken()}`
+      Authorization: `Bearer ${utils.getToken()}`
     },
     body: JSON.stringify(payload)
-  })
+  });
 }
 
 /**
  * Creates a HTTP request that fetches a collection of courses.
  * @returns Promise<Object>
  */
-function fetchCourses () {
+function fetchCourses() {
   return fetch(prefixHostAddress('/v1/courses'), {
     method: 'get',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json'
     }
-  })
+  });
 }
 
 /**
  * Creates a HTTP request that fetches a collection of courses.
  * @returns Promise<Object>
  */
-function fetchCourseModules (courseId:number) {
+function fetchCourseModules(courseId: number) {
   return fetch(prefixHostAddress(`/v1/courses/${courseId}/modules`), {
     method: 'get',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json'
     }
-  })
+  });
 }
 
 /**
  * Creates a user http request.
  */
-function authenticateUser (user:Object) {
+function authenticateUser(user: Object) {
   return fetch(prefixHostAddress('/v1/login'), {
     method: 'POST',
     headers: {
-      'Accept': 'application/json',
+      Accept: 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(user)
-  })
+  });
 }
 
 /**
  * Checks the status of a request.
- * 
+ *
  * @param {Object} response
  */
-function checkStatus (response:any) {
+function checkStatus(response: any) {
   if (response.status >= 200 && response.status < 401) {
     return Promise.resolve(response);
   } else {
@@ -205,14 +213,20 @@ function checkStatus (response:any) {
 
 /**
  * Sets an auth token.
- * 
+ *
  * @param {Object<Response>} response
  * @return {Object<Response>}
  */
-function setToken (response:any) {
+function setToken(response: any) {
   if (response.credentials) {
-    localStorage.setItem('token', JSON.stringify({ token: response.credentials.token }));
-    localStorage.setItem('refreshToken', JSON.stringify({ refreshToken: response.credentials.refreshToken }));
+    localStorage.setItem(
+      'token',
+      JSON.stringify({ token: response.credentials.token })
+    );
+    localStorage.setItem(
+      'refreshToken',
+      JSON.stringify({ refreshToken: response.credentials.refreshToken })
+    );
   }
 
   return response;
@@ -220,11 +234,11 @@ function setToken (response:any) {
 
 /**
  * Sets the auth user.
- * 
+ *
  * @param {Object<Response>} response
  * @return {Object<Response>}
  */
-function setUser (response:any) {
+function setUser(response: any) {
   if (response.user) {
     localStorage.setItem('user', JSON.stringify({ user: response.user }));
   }
@@ -234,22 +248,26 @@ function setUser (response:any) {
 
 /**
  * Checks the status of a request.
- * 
+ *
  * @param {Object} response
  */
-function errorHandler (dispatch:Dispatch, e:any) {
-  console.log('Sorry, we encountered an error trying to process your request. Please try again.');
+function errorHandler(dispatch: Dispatch, e: any) {
+  console.log(
+    'Sorry, we encountered an error trying to process your request. Please try again.'
+  );
 
-  dispatch(AppActions.Map({
-    hasRequestError: true
-  }))
+  dispatch(
+    AppActions.Map({
+      hasRequestError: true
+    })
+  );
 }
 
-function toText(response:any) {
+function toText(response: any) {
   return response.text();
 }
 
-function toJSON(response:any) {
+function toJSON(response: any) {
   return response.json();
 }
 
@@ -271,5 +289,5 @@ export default {
   toText,
   toJSON,
   updateUserModule,
-  updateUserLesson,
-}
+  updateUserLesson
+};
